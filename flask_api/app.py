28d245e9f773852f06358dbf68bfd72e859197a6
Shahrukh Ilyas <shahrukh_ilyas@outlook.com>
2024-07-09 00:58:01 +0100

    DEBUGGED MIGRATIONS ISSUES OS LOCAL ENVIRONMENT

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,11 +1,11 @@
+# flask_api/app.py
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import config
-from controllers.user_controller import user_bp
+from config import Config
 from dotenv import load_dotenv
+from flask_migrate import Migrate
 import os
-app = Flask(__name__)
-
 
 # Load environment variables based on FLASK_ENV
 if os.getenv('FLASK_ENV') == 'production':
@@ -13,32 +13,21 @@ if os.getenv('FLASK_ENV') == 'production':
 else:
     load_dotenv('.env')       # Load development environment variables
 
-#env_config = config[os.getenv('FLASK_ENV') or 'development']
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
-
+    app.config.from_object(Config)
     db.init_app(app)
-
-    with app.app_context():
-        db.create_all()
-
-    app.register_blueprint(user_bp, url_prefix='/api')
-    
     migrate.init_app(app, db)
-
-    return app
-
-
-# Optional: Define a CLI command to initialize the database
-@app.cli.command()
-def init_db():
+    
     with app.app_context():
+        from controllers.user_controller import user_bp
+        app.register_blueprint(user_bp, url_prefix='/api')
         db.create_all()
+    
+    return app
 
 if __name__ == '__main__':
     app = create_app()
--- flask_api/controllers/user_controller.py
+++ flask_api/controllers/user_controller.py
@@ -1,16 +1,17 @@
-from flask import Blueprint, request, jsonify
-from ..app import db
-from models import User
+from flask import Blueprint, request, jsonify,current_app
+from models.user import User
 
 user_bp = Blueprint('user_bp', __name__)
 
 @user_bp.route('/users', methods=['GET'])
 def get_users():
+    db = current_app.db
     users = User.query.all()
     return jsonify([user.username for user in users])
 
 @user_bp.route('/users', methods=['POST'])
 def create_user():
+    db = current_app.db
     data = request.get_json()
     new_user = User(username=data['username'], email=data['email'], password=data['password'])
     db.session.add(new_user)
